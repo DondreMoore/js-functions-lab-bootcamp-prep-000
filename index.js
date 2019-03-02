79838f580014734fdb8fef677f7dc8bf7ea97af8
Dondre Moore <raptureimmortal@icloud.com>
2019-03-02 20:43:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ function happyHolidayTo(You, Mom) {
   return 'Happy Independence Day, you!'
 }
 function holidayCountdown() {
-  console.log('${days}')
-  console.log('${holiday}')
+  oliday}')
   return(`It's ${3} days until ${holiday}!`)
 }
 
